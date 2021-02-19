1f7074e218bf98063f592af19379831e52656483
Joseph Yu <44690832+JosephZYU@users.noreply.github.com>
2021-02-19 12:34:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ homedir=$1
 # dotfiles directory
 dotfiledir=${homedir}/dotfiles
 
-# list of files/folders to symlink in ${homedir}
-files="bash_profile bashrc bash_prompt aliases private"
+# list of files/folders to symlink in ${homedir} # private
+files="bash_profile bashrc bash_prompt aliases"
 
 # change to the dotfiles directory
 echo "Changing to the ${dotfiledir} directory"
